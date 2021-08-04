127d652ce377a83f6887dbc01b355f9f16ae89f5
Eric Choi <ericchoirr38@gmail.com>
2021-08-04 23:03:55 +0000

    firebase stuff

--- rateDiningHall/public/js/displayCard.js
+++ rateDiningHall/public/js/displayCard.js
@@ -1,9 +1,24 @@
-const getNotes = (userId) => {
+const getNotes = () => {
     console.log("entered");
-  const notesRef = firebase.database().ref(`colleges`);
+  const notesRef = firebase.database().ref(`colleges/UCLA/dininghall/Rieber Court`);
   notesRef.on('value', (snapshot) => {
+    
     const data = snapshot.val();
-    console.log(data);
+    //console.log(data[-MgIAKBYEYEMF0mu77oe].Meal);
+    /*
+    let name = data[0].name;
+    console.log("name: " + name);
+    let diningHall = name.database().ref('dininghall');
+    let pussy = diningHall[0];
+    console.log("pussy: "+pussy);
+    const picRef = firebase.database().ref('collegephoto');
+    let photo;
+    for(let i = 0; i < picRef.lenth; i++)
+    {
+        if(picRef[i] == name)
+            photo = picRef[i].Picture;
+    }
+    */
     renderDataAsHtml(data);
   });
 };
@@ -11,12 +26,14 @@ const getNotes = (userId) => {
 const renderDataAsHtml = (data) => {
   let cards = ``;
   for(const noteItem in data) {
+    console.log(noteItem);
     const note = data[noteItem];
+    console.log(note.Meal);
     // For each note create an HTML card
-    cards += createCard(note, noteItem)
+    //cards += createCard(note, noteItem)
   };
   // Inject our string of HTML into our viewNotes.html page
-  document.querySelector('#app').innerHTML = cards;
+  //document.querySelector('#app').innerHTML = cards;
 };
 
 getNotes();
