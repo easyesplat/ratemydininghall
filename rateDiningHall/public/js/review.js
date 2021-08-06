d6f8eec45e04a25f6d4427b78c6c9fe580e4b3c6
AlannaFey <alanna.fey.hill@gmail.com>
2021-08-06 04:26:34 +0000

    yay

--- rateDiningHall/public/js/review.js
+++ rateDiningHall/public/js/review.js
@@ -1,16 +1,16 @@
 let googleUser;
 
-//window.onload = (event) => {
-    // Use this to retain user state between html pages.
-   // firebase.auth().onAuthStateChanged(function(user) {
-    //    if (user) {
-    //        console.log('Logged in as: ' + user.displayName);
-   //         googleUser = user;
-   //     } else {
- //           window.location = 'index.html'; // If not logged in, navigate back to login page.
-  //      }
-  //  });
-//};
+window.onload = (event) => {
+     //Use this to retain user state between html pages.
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('Logged in as: ' + user.displayName);
+            googleUser = user;
+        } else {
+            window.location = 'index.html'; // If not logged in, navigate back to login page.
+        }
+    });
+};
 console.log("js loaded");
 
 /*const button = document.querySelector("#subButton");
